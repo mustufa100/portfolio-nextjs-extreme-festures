67e5fb823b27ffc153b8126b061b8c3dbac81b0f
mustufa100 <57327694+mustufa100@users.noreply.github.com>
2024-07-15 19:39:42 +0500

    nextjs-portfolio-components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { navItems } from "@/data";
+
+import Hero from "@/app/components/Hero";
+import Grid from "@/app/components/Grid";
+import Footer from "@/app/components/Footer";
+import Clients from "@/app/components/Clients";
+import Approach from "@/app/components/Approach";
+import Experience from "@/app/components/Experience";
+import RecentProjects from "@/app/components/RecentProjects";
+import { FloatingNav } from "@/app/components/ui/FloatingNav";
+
+const Home = () => {
   return (
-    <main>
-      
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
